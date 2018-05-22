608ff92bf35290c2aa86f2977ca23fe536e691c3
Medivh He <medivh.he@zoom.us>
2018-05-22 18:08:37 +0800

    project swagger api config

--- rest-api/src/main/java/com/github/crab2died/restapi/controller/ApiController.java
+++ rest-api/src/main/java/com/github/crab2died/restapi/controller/ApiController.java
@@ -1,17 +1,19 @@
 package com.github.crab2died.restapi.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.annotations.ApiIgnore;
 
-@Api
+@Api(value = "API Home", tags = {"API Home"})
 @Controller
 public class ApiController {
 
     @ApiIgnore
     @GetMapping("/")
+    @ApiOperation(value = "Api Home")
     public String apiHome() {
 
         return "redirect:swagger-ui.html";
