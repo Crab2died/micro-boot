190fad7bf28b627040cd7e0db8f8bde8a1d8065b
Medivh He <medivh.he@zoom.us>
2018-05-23 18:04:42 +0800

    redis config

--- rest-api/src/main/java/com/github/crab2died/restapi/controller/SystemController.java
+++ rest-api/src/main/java/com/github/crab2died/restapi/controller/SystemController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Api(value = "System Info", tags = {"System Info", })
+@Api(value = "System Info", tags = {"System Info",})
 public class SystemController {
 
     @Autowired
@@ -22,21 +22,21 @@ public class SystemController {
     public ApplicationContext ctx;
 
     @RequestMapping(value = "/sys/hello", method = RequestMethod.GET,
-            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, },
-            consumes = {MediaType.ALL_VALUE, }
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE,},
+            consumes = {MediaType.ALL_VALUE,}
     )
     @ResponseBody
     @ApiOperation(value = "Say Hello", notes = "hello note", httpMethod = "GET")
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(value = "Your Name", name = "name", paramType = "query", defaultValue = "Crab2Died", dataTypeClass = String.class ),
-            @ApiImplicitParam(value = "Say Hello Pls.", name = "sayHello", paramType = "query", defaultValue = "Hello, Friend", dataType = "string" )
+            @ApiImplicitParam(value = "Your Name", name = "name", paramType = "query", defaultValue = "Crab2Died", dataTypeClass = String.class),
+            @ApiImplicitParam(value = "Say Hello Pls.", name = "sayHello", paramType = "query", defaultValue = "Hello, Friend", dataType = "string")
     })
     @ApiResponses(value = {
             @ApiResponse(code = org.apache.http.HttpStatus.SC_OK, message = "success request", response = HelloApi.class),
             @ApiResponse(code = org.apache.http.HttpStatus.SC_BAD_REQUEST, message = "bad request")
     })
-    public ResponseEntity<HelloApi> sayHello(@ApiParam(value = "Hello Model", name = "hello api") HelloApi helloApi){
-        if (null == helloApi){
+    public ResponseEntity<HelloApi> sayHello(@ApiParam(value = "Hello Model", name = "hello api") HelloApi helloApi) {
+        if (null == helloApi) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(helloApi, HttpStatus.OK);
@@ -45,7 +45,7 @@ public class SystemController {
     @GetMapping("/sys/ctx")
     @ResponseBody
     @ApiOperation(value = "App Context")
-    public Object getAppContext(){
+    public Object getAppContext() {
 
         testService.redisTest();
         return ctx.getBeanDefinitionNames();
