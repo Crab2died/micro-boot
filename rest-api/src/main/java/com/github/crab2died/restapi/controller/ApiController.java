72b02ff1a85d4c9c3f8835897470878c1d6205b9
Crab2Died <hwb9588@126.com>
2018-06-30 09:36:47 +0800

    ppt

--- rest-api/src/main/java/com/github/crab2died/restapi/controller/ApiController.java
+++ rest-api/src/main/java/com/github/crab2died/restapi/controller/ApiController.java
@@ -6,11 +6,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.Date;
 
 @Api(value = "API Home", tags = {"API Home"})
 @Controller
@@ -34,7 +37,7 @@ public class ApiController {
         return System.getProperties();
     }
 
-    @PostMapping("say-hello")
+    @PostMapping("hello")
     @ResponseBody
     @ApiOperation(value = "Say Hello")
     public ResponseEntity<Void> sayHello(@RequestBody @ApiParam(value = "Hello Api Body", name = "helloApi") HelloApi helloApi) {
@@ -46,6 +49,13 @@ public class ApiController {
     @ResponseBody
     @ApiOperation(value = "Hello")
     public ResponseEntity<HelloApi> hello(@PathVariable @ApiParam(value = "ID.", name = "id") String id) {
-        return new ResponseEntity<>(helloApiService.getHello(id), HttpStatus.OK);
+
+        // id 424f5528-1e4b-4b58-b1c6-0171b1cd2220
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("ETag", "341567381");
+
+        headers.add("Last-Modified", "Fri, 29 Jun 2018 08:10:59 GMT");
+        headers.add("Cache-Control", "max-age=5");
+        return new ResponseEntity<>(helloApiService.getHello(id), headers, HttpStatus.OK);
     }
 }
